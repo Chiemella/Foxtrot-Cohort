36cc2b34941d6bc27873094d7ba2d102085942f1
Chiemella <williamschiemelaod@gmail.com>
2025-08-05 18:51:42 -0700

    PYTHON NUMBERS

--- python_numbers.py
+++ python_numbers.py
@@ -18,3 +18,29 @@ z = 1232341543567
 print(type(x))
 print(type(y))
 print(type(z))
+
+
+"""
+Float
+Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+
+Float can also be scientific numbers with an "e" to indicate the power of 10.
+"""
+i = 0.9
+j = -0999988.67897890
+k = +90.12345
+print(type(i))
+print(type(j))
+print(type(k))
+
+
+"""
+Complex
+Complex numbers are written with a "j" as the imaginary part:
+"""
+d = 3+5j
+e = 5j
+f = -5j
+print(type(d))
+print(type(e))
+print(type(f))
