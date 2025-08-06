e08cedb4e99deab68789c5773cea9faf8ed07881
Chiemella <williamschiemelaod@gmail.com>
2025-08-05 18:21:56 -0700

    adding int

---
+++ python_numbers.py
@@ -0,0 +1,20 @@
+"""
+There are three numeric types in Python:
+
+int
+float
+complex
+"""
+
+
+
+"""
+Int
+Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+"""
+x = 1
+y = 12
+z = 1232341543567
+print(type(x))
+print(type(y))
+print(type(z))
