5cbccad3fd00dbf27dc82b202c00a62f02e73ba5
MAC <mac@MACs-MacBook-Pro.local>
2025-10-02 11:47:22 +0100

    updated

---
+++ week_1/day_2.py
@@ -0,0 +1,26 @@
+# COMMENTS
+
+# This is a single line comment use for documentations.
+
+'''
+This is a multiline comment.
+You can see me write text in a new line.
+This is use for documentations.
+'''
+
+
+# Variables & Data-types: a simople container that holds a value
+bottle = "water"
+
+#Data Types: specifically of characters
+"Friends" # String
+
+122345667 # Integers
+
+1233.5876 # Floats
+
+True
+False # Boolean
+
+
+first_name = "" # Snake case
